0269bb24c569e39dca086de88f3ac5e7fd078701
jordanhailey <jordan.m.hailey@gmail.com>
2021-08-09 12:41:42 -0700

    refactor parseBlock to build array from child nodes on each iteration, enabling use of .map()

--- .eleventy/filters/parseBlock.js
+++ .eleventy/filters/parseBlock.js
@@ -1,16 +1,3 @@
-// {% set c = node.container if node.container else "" %}{% if c == "br" or c == "hr"%}<{{ c | safe }}/>{% elif not c%}{{node | trim}}
-// {% else %}
-// {% set children %}{% for node in node.nodes %}{% include './node.html' %}{% endfor %}{% endset %}
-// {% set hasAside = false %}
-// {% for child in children %}{%set hasAside = true if child.container == "aside" else hasAside %}{% endfor %}
-// {% set attrs %}
-// {% for i in node.attrs %}
-// {% for k,v in i %}{{k|safe}}="{{v|safe}}"{% endfor %}
-// {% endfor %}
-// {% endset %}
-// <{{ c | safe }} {{ attrs | safe}}>{{ children | trim | safe }}</{{ c | safe }}>
-// {% endif %}
-
 const nonNullNonEmptyObject = (obj) => typeof obj == "object" && obj !== null && obj !== {}
 function parseAttributes(attributes,isStyleAttr=false) {
   let attrs = "";
@@ -32,20 +19,18 @@ function parseAttributes(attributes,isStyleAttr=false) {
   return attrs;
 }
 
-
 function parseBlock(block) {
   if (!block) return block;
-  const {tagName,attributes,nodes} = block;
+  const {tagName,attributes,nodes:n} = block;
+  const nodes = Array.from(n);
   if (!tagName && block) return block;
   const attrs = parseAttributes(attributes);
-  // const attrs = attributes?.map(attr => {
-  //   return Object.entries(attr).map(([k,v]) => `${k}=${typeof v == "object" ? JSON.stringify(v).replace(/"/g,"'") : v}`)
-  // }).join(" ") || "";
   if (/^[hb]r$/.test(tagName)) return `<${tagName}>`
   else if (/^input$/.test(tagName)) return `<${tagName} ${attrs} />`
-  const inside = nodes?.map(node=>parseBlock(node)).join("") || "";
-  const output = `<${tagName} ${attrs} >${inside||""}</${tagName}>`;
-  // console.log({output})
-  return output;
+  if (nodes.length) {
+    const inside = nodes.map(node=>parseBlock(node)).join("") || "";
+    const output = `<${tagName} ${attrs} >${inside||""}</${tagName}>`;
+    return output;
+  } else return block;
 }
 module.exports = parseBlock;
