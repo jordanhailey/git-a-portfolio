8fdf169dd14dd738bd96e9de1fad161e814c2e30
Jordan Hailey <jordan.m.hailey@gmail.com>
2021-08-17 12:40:47 -0700

    revert last commit. TODO work on override in local env, permalink resolves to falsy value landing users on 404

--- src/pages/pages.11tydata.js
+++ src/pages/pages.11tydata.js
@@ -1,6 +1,6 @@
 function determinePermalink(data){
   const {permalink,page} = data;
-  if (permalink !== undefined) return permalink;
+  if (permalink) return permalink;
   if (page.filePathStem.substr(-5) == "index") return `${page.filePathStem.substr(6,page.filePathStem.length-11)}`
   return `${page.filePathStem.substr(6)}/`;
 }
